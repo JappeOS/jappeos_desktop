2aa7ebcdf41549fe46e97cc0e26cab99f1f96218
Jappe02 <jasper.mannonen@gmail.com>
2026-02-05 21:21:11 +0200

    Add new topbar button and topbar widgets as a result of new/improved menu system

--- lib/base/components/desktop_widgets.dart
+++ lib/base/components/desktop_widgets.dart
@@ -1,5 +1,5 @@
 //  JappeOS-Desktop, The desktop environment for JappeOS.
-//  Copyright (C) 2025  Jappe02
+//  Copyright (C) 2026  The JappeOS team.
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as
@@ -68,7 +68,7 @@ class _DApplicationItemState extends State<DApplicationItem> {
       height: height,
       child: RepaintBoundary(
         child: Tooltip(
-          tooltip: (_) => Text(widget.title),
+          tooltip: (_) => TooltipContainer(child: Text(widget.title)),
           child: MouseRegion(
             onEnter: (p0) => setState(() => _isHovered = true),
             onExit: (p0) => setState(() => _isHovered = false),
@@ -151,7 +151,7 @@ class DTopbarButton extends StatefulWidget {
 
   final AlignmentGeometry alignment;
 
-  const DTopbarButton._(
+  const DTopbarButton(
       {Key? key, this.title, this.children = const [], required this.menuControllerRef, required this.menu, this.alignment = Alignment.center})
       : super(key: key);
 
@@ -161,7 +161,7 @@ class DTopbarButton extends StatefulWidget {
     required DesktopMenu menu,
     AlignmentGeometry alignment = Alignment.center,
   }) {
-    return DTopbarButton._(
+    return DTopbarButton(
       menuControllerRef: menuControllerRef,
       menu: menu,
       alignment: alignment,
@@ -175,7 +175,7 @@ class DTopbarButton extends StatefulWidget {
     required DesktopMenu menu,
     AlignmentGeometry alignment = Alignment.center,
   }) {
-    return DTopbarButton._(
+    return DTopbarButton(
       menuControllerRef: menuControllerRef,
       menu: menu,
       alignment: alignment,
@@ -189,7 +189,7 @@ class DTopbarButton extends StatefulWidget {
     required DesktopMenu menu,
     AlignmentGeometry alignment = Alignment.center,
   }) {
-    return DTopbarButton._(
+    return DTopbarButton(
       menuControllerRef: menuControllerRef,
       menu: menu,
       alignment: alignment,
@@ -206,7 +206,7 @@ class DTopbarButton extends StatefulWidget {
     required DesktopMenu menu,
     AlignmentGeometry alignment = Alignment.center,
   }) {
-    return DTopbarButton._(
+    return DTopbarButton(
       menuControllerRef: menuControllerRef,
       menu: menu,
       alignment: alignment,
@@ -301,6 +301,141 @@ class _DTopbarButtonState extends State<DTopbarButton> {
   }
 }
 
+/// A topbar button that captures the global position of the button and is used
+/// to open a desktop menu.
+class DTopbarButtonNew extends StatefulWidget {
+  final String? title;
+  final bool isSelected;
+  final List<Widget> children;
+  final AlignmentGeometry alignment;
+  final LogicalKeySet? shortcut;
+  final void Function(Offset)? onPressed;
+
+  const DTopbarButtonNew({
+    Key? key,
+    this.title,
+    this.isSelected = false,
+    this.children = const [],
+    this.alignment = Alignment.center,
+    this.shortcut,
+    this.onPressed
+  }) : super(key: key);
+
+  @override
+  _DTopbarButtonNewState createState() => _DTopbarButtonNewState();
+}
+
+class _DTopbarButtonNewState extends State<DTopbarButtonNew> {
+  static const double _kIconSize = 17;
+  static const double _kHeight = 26;
+
+  bool _init = false;
+  Offset _globalPosition = Offset.zero;
+
+  bool _hovering = false;
+  final _borderRad = BorderRadius.circular(100);
+  final _borderWidth = 1.0;
+
+  @override
+  void didChangeDependencies() {
+    if (_init) return;
+    _init = true;
+
+    if (widget.shortcut != null) {
+      final keybinds = GlobalKeybindScope.of(context);
+      keybinds.register(
+        widget.shortcut!,
+        () {
+          setState(() {});
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            widget.onPressed?.call(_globalPosition);
+          });
+          return true;
+        },
+      );
+    }
+
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    if (widget.shortcut != null) {
+      final keybinds = GlobalKeybindScope.of(context);
+      keybinds.unregister(widget.shortcut!);
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final borderColor = theme.colorScheme.border;
+
+    return Container(
+      margin: const EdgeInsets.only(left: 5, right: 5),
+      alignment: widget.alignment,
+      height: _kHeight,
+      decoration: BoxDecoration(
+        borderRadius: _borderRad,
+        color: _hovering || widget.isSelected
+            ? theme.colorScheme.secondary.scaleAlpha(theme.surfaceOpacity ?? 0)
+            : Colors.transparent,
+        border: _hovering || widget.isSelected
+            ? Border.all(width: _borderWidth, color: borderColor)
+            : Border.all(width: _borderWidth, color: Colors.transparent),
+      ),
+      child: RepaintBoundary(
+        child: Builder(
+          builder: (context) {
+            // Use a post frame callback to ensure the widget has been laid out
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              final renderBox = context.findRenderObject() as RenderBox;
+              final global = renderBox.localToGlobal(Offset.zero);
+              _globalPosition = Offset(
+                global.dx + renderBox.size.width / 2,
+                global.dy,
+              );
+            });
+
+            return MouseRegion(
+              onEnter: (value) => setState(() {
+                _hovering = true;
+              }),
+              onExit: (value) => setState(() {
+                _hovering = false;
+              }),
+              child: GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {
+                  widget.onPressed?.call(_globalPosition);
+                },
+                child: SizedBox(
+                  height: _kHeight,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      left: 6 * theme.scaling,
+                      right: 6 * theme.scaling,
+                    ),
+                    child: IconTheme(
+                      data: IconTheme.of(context).copyWith(size: _kIconSize),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        spacing: 6 * theme.scaling,
+                        children: widget.children,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
 /// A background widget for desktop menus.
 class DMenuBackground extends StatelessWidget {
   final Widget child;
@@ -383,7 +518,6 @@ class _DWindowViewState extends State<DWindowView> {
 
   @override
   Widget build(BuildContext context) {
-    final borderRadius = Theme.of(context).borderRadiusMd;
     final width = widget.height * widget.aspectRatio;
 
     return Column(
@@ -519,12 +653,71 @@ class _DesktopTopBarState extends State<DesktopTopBar> {
         child: Row(
           children: [
             DTopbarButton.icon(icon: Icons.apps, menuControllerRef: widget.menuController, menu: LauncherMenu()),
-            DTopbarButton.icon(icon: Icons.search, menuControllerRef: widget.menuController, menu: SearchMenu()),
-            DTopbarButton.icon(icon: Icons.menu_open, menuControllerRef: widget.menuController, menu: OpenWindowsMenu()),
+            //DTopbarButton.icon(icon: Icons.search, menuControllerRef: widget.menuController, menu: SearchMenu()),
+            DTopbarButton.icon(icon: Icons.menu_open, menuControllerRef: widget.menuController, menu: OverviewMenu()),
             const Spacer(),
             DTopbarButton.icons(icons: const [Icons.mic, Icons.camera], menuControllerRef: widget.menuController, menu: PermissionsMenu()),
-            DTopbarButton.icons(icons: const [Icons.wifi, Icons.volume_mute, Icons.battery_full], menuControllerRef: widget.menuController, menu: ControlCenterMenu()),
+            //DTopbarButton.icons(icons: const [Icons.wifi, Icons.volume_mute, Icons.battery_full], menuControllerRef: widget.menuController, menu: ControlCenterMenu()),
             DTopbarButton.textAndIcon(icon: Icons.notifications, text: _timeString, menuControllerRef: widget.menuController, menu: NotificationMenu()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// The bar that shows system information and provides access to important parts
+/// of the OS, like notifications and quick settings.
+class DesktopTopBarNew extends StatelessWidget {
+  final DesktopMenuRegistry registry;
+  final DesktopMenuController menuController;
+
+  const DesktopTopBarNew({
+    super.key,
+    required this.registry,
+    required this.menuController,
+  });
+
+  Widget _button(BuildContext context, DesktopMenuEntry entry) {
+    return ListenableBuilder(
+      listenable: entry,
+      builder: (_, __) {
+        return DTopbarButtonNew(
+          //tooltip: entry.label, TODO
+          isSelected: entry.isOpen,
+          shortcut: entry.shortcut,
+          onPressed: (offset) => entry.toggle(menuController, offset),
+          children: entry.buildIcon(context),
+        );
+      },
+    );
+  }
+
+  List<Widget> _createLeftSide(BuildContext context) {
+    return registry.entries
+        .where((e) => e.type == DesktopMenuEntryType.launcher)
+        .map((entry) => _button(context, entry)).toList();
+  }
+
+  List<Widget> _createRightSide(BuildContext context) {
+    return registry.entries
+        .where((e) => e.type == DesktopMenuEntryType.tray)
+        .map((entry) => _button(context, entry)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      height: DSKTP_UI_LAYER_TOPBAR_HEIGHT,
+      child: DBlurContainer(
+        child: Row(
+          children: [
+            ..._createLeftSide(context),
+            const Spacer(),
+            ..._createRightSide(context),
           ],
         ),
       ),
