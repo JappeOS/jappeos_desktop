595af1b3b84663897faeda5e67726a487205153e
Jappe02 <jasper.mannonen@gmail.com>
2026-02-24 19:48:23 +0200

    Create desktop entry provider

--- lib/src/provider/desktop_entry_provider.dart
+++ lib/src/provider/desktop_entry_provider.dart
@@ -14,34 +14,77 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import 'dart:io';
+
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 import 'package:freedesktop_desktop_entry/freedesktop_desktop_entry.dart';
 
-// TODO: Parse desktop entries properly and only when logged in.
 class DesktopEntryProvider extends ChangeNotifier {
-  List<DesktopEntry> entries = [];
+  final _themes = FreedesktopIconThemes();
+  Map<String, DesktopEntry> _entries = {};
+  bool _isDirty = true;
 
   DesktopEntryProvider() {
-    _loadEntries();
-    _watchDirectories();
+    _themes.loadThemes();
   }
 
-  void _loadEntries() {
-    throw UnimplementedError();
-    // Scan all known freedesktop paths
-    //entries = parseAllDesktopEntries(); TODO
+  Future<Map<String, DesktopEntry>> getEntries() async {
+    if (_isDirty) {
+      try {
+        await _reloadEntries();
+      } catch (e) {
+        // Handle errors, e.g., log them
+      }
+      _isDirty = false;
+    }
+    return _entries;
+  }
+
+  Future<File?> getIcon(DesktopEntry entry) async {
+    String? icon = entry.entries[DesktopEntryKey.icon.string]?.value;
+    if (icon == null) return null;
+    return _themes.findIcon(
+      IconQuery(
+        name: icon,
+        size: 64,
+        extensions: ['png'],
+      ),
+    );
+  }
+
+  Future<bool> launchDesktopEntry(DesktopEntry desktopEntry) async {
+    String? exec = desktopEntry.entries[DesktopEntryKey.exec.string]?.value;
+    if (exec == null) {
+      return false;
+    }
+    final bool terminal = desktopEntry.entries[DesktopEntryKey.terminal.string]?.value.getBoolean() ?? false;
+    return _launchApplication(command: exec, terminal: terminal);
+  }
+
+  Future<bool> _launchApplication({
+    required String command,
+    bool terminal = false,
+  }) async {
+    // FIXME
+    command = command.replaceAll(RegExp(r'( %.?)'), '');
+    debugPrint("Launching $command");
+
+    try {
+      if (terminal) {
+        await Process.start('kgx', ['-e', command]);
+      } else {
+        await Process.start('/bin/sh', ['-c', command]);
+      }
+      return true;
+    } on ProcessException catch (e) {
+      stderr.writeln(e.toString());
+      return false;
+    }
+  }
+
+  Future<void> _reloadEntries() async {
+    final entries = await parseAllInstalledDesktopFiles();
+    _entries = entries;
     notifyListeners();
-  }
-
-  void _watchDirectories() {
-    throw UnimplementedError();
-/*
-    for (final dir in freedesktopDirs) {
-      final watcher = DirectoryWatcher(dir);
-      watcher.events.listen((event) {
-        _loadEntries(); // Naive way: reload all
-        // For optimization, parse only affected file
-      });
-    }*/
   }
 }
