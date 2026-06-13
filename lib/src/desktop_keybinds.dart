b2eff1a65e58a07815f46b503bce51a22ce8f19c
Jappe02 <jasper.mannonen@gmail.com>
2026-06-14 01:42:45 +0300

    W.I.P keybind handling stuff

--- lib/src/desktop_keybinds.dart
+++ lib/src/desktop_keybinds.dart
@@ -4,17 +4,23 @@ import 'package:jappeos_services/jappeos_services.dart';
 import 'package:provider/provider.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
+import 'desktop_menu_manager/desktop_menu_controller.dart';
+import 'desktop_menu_manager/desktop_menu_registry.dart';
+import 'desktop_menu_manager/menus/switcher_menu.dart';
+
 class DesktopKeybinds extends StatefulWidget {
+  final DesktopMenuRegistry registry;
+  final DesktopMenuController menuController;
   final Widget child;
 
-  const DesktopKeybinds({super.key, required this.child});
+  const DesktopKeybinds({super.key, required this.child, required this.registry, required this.menuController});
 
   @override
   State<DesktopKeybinds> createState() => _DesktopKeybindsState();
 }
 
 class _DesktopKeybindsState extends State<DesktopKeybinds> {
-  final sliderSteps = [
+  final _sliderSteps = [
     0.0,
     0.01, 0.02, 0.03, 0.05, 0.07,
     0.1, 0.15, 0.25, 0.3,
@@ -28,32 +34,42 @@ class _DesktopKeybindsState extends State<DesktopKeybinds> {
   final _kKeyAudioVolumeUp = LogicalKeySet(LogicalKeyboardKey.audioVolumeUp);
   final _kKeyAudioMicrophoneToggle = LogicalKeySet(LogicalKeyboardKey.microphoneToggle);
   final _kKeyAudioMicrophoneMute = LogicalKeySet(LogicalKeyboardKey.microphoneVolumeMute);
+  final _kKeyDmenuSwitcherOpen = LogicalKeySet(LogicalKeyboardKey.alt, LogicalKeyboardKey.tab);
+
+  bool _altPressed = false;
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
       init();
     });
   }
 
   void init() {
+    ServicesBinding.instance.keyboard.addHandler(_handleAltKey);
+
     final keybinds = GlobalKeybindScope.of(context);
     keybinds.register(_kKeyAudioMute, _audioHandleMute);
     keybinds.register(_kKeyAudioVolumeDown, _audioHandleVolumeDown);
     keybinds.register(_kKeyAudioVolumeUp, _audioHandleVolumeUp);
     keybinds.register(_kKeyAudioMicrophoneToggle, _audioHandleMicrophoneToggle);
     keybinds.register(_kKeyAudioMicrophoneMute, _audioHandleMicrophoneToggle);
+    keybinds.register(_kKeyDmenuSwitcherOpen, _dmenuSwitcherHandleOpen);
   }
 
   @override
   void dispose() {
+    ServicesBinding.instance.keyboard.removeHandler(_handleAltKey);
+
     final keybinds = GlobalKeybindScope.of(context);
     keybinds.unregister(_kKeyAudioMute, _audioHandleMute);
     keybinds.unregister(_kKeyAudioVolumeDown, _audioHandleVolumeDown);
     keybinds.unregister(_kKeyAudioVolumeUp, _audioHandleVolumeUp);
     keybinds.unregister(_kKeyAudioMicrophoneToggle, _audioHandleMicrophoneToggle);
     keybinds.unregister(_kKeyAudioMicrophoneMute, _audioHandleMicrophoneToggle);
+    keybinds.unregister(_kKeyDmenuSwitcherOpen, _dmenuSwitcherHandleOpen);
     super.dispose();
   }
 
@@ -61,8 +77,8 @@ class _DesktopKeybindsState extends State<DesktopKeybinds> {
     int closest = 0;
     double minDiff = double.infinity;
 
-    for (int i = 0; i < sliderSteps.length; i++) {
-      final diff = (sliderSteps[i] - value).abs();
+    for (int i = 0; i < _sliderSteps.length; i++) {
+      final diff = (_sliderSteps[i] - value).abs();
       if (diff < minDiff) {
         minDiff = diff;
         closest = i;
@@ -75,14 +91,29 @@ class _DesktopKeybindsState extends State<DesktopKeybinds> {
     int index = _findClosestIndex(currentValue);
 
     index += direction;
-    index = index.clamp(0, sliderSteps.length - 1);
+    index = index.clamp(0, _sliderSteps.length - 1);
 
-    return sliderSteps[index];
+    return _sliderSteps[index];
   }
 
   @override
   Widget build(BuildContext context) {
     return widget.child;
+  }
+
+  bool _handleAltKey(KeyEvent event) {
+    if (event.logicalKey == LogicalKeyboardKey.alt ||
+        event.logicalKey == LogicalKeyboardKey.altLeft ||
+        event.logicalKey == LogicalKeyboardKey.altRight) {
+      if (event is KeyDownEvent) {
+        _altPressed = true;
+      }
+      if (event is KeyUpEvent) {
+        _altPressed = false;
+        return _dmenuSwitcherHandleClose();
+      }
+    }
+    return false;
   }
 
   bool _audioHandleMute() {
@@ -114,6 +145,24 @@ class _DesktopKeybindsState extends State<DesktopKeybinds> {
     final device = audio.activeInputDevice;
     if (device == null) return true;
     audio.setDeviceMuted(device, !device.muted);
+    return true;
+  }
+
+  bool _dmenuSwitcherHandleOpen() {
+    // TODO
+    print("TODO: Open dmenu switcher");
+    widget.menuController.openMenu(
+          widget.registry.entries
+              .firstWhere((e) => e is SwitcherMenuEntry)
+              .createMenu()
+    );
+    return true;
+  }
+
+  bool _dmenuSwitcherHandleClose() {
+    // TODO
+    print("TODO: Close dmenu switcher");
+    widget.menuController.closeMenu();
     return true;
   }
 }
